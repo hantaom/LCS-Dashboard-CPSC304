e15a4ba5d63827dc570902755b1adde9c716ba6e
hantaom <hantaom@gmail.com>
2018-03-24 05:48:53 -0700

    Changed index file to heroku build

--- index.js
+++ index.js
@@ -16,12 +16,35 @@ const QueryHandler = require('./query.js');
 
 // Hantao PostgreSQL code
 // ######################################################################################################
-const connectionString = process.env.DATABASE_URL || 'postgres://hantao:Password1@localhost:5432/lcs';
-console.log(connectionString);
-const pg = require('pg');
-const client = new pg.Client(connectionString);
+// const connectionString = process.env.DATABASE_URL || 'postgres://hantao:Password1@localhost:5432/lcs';
+// console.log(connectionString);
+// const pg = require('pg');
+// const client = new pg.Client(connectionString);
+// client.connect();
+// // console.log(client.log);
+// let queryResults = [];
+// client.query('SELECT * FROM PLAYERS;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     // console.log(JSON.stringify(row));
+//     queryResults.push(row["pl_name"]);
+//   }
+//   // console.log(JSON.stringify(queryResults));
+//   client.end();
+// });
+// ######################################################################################################
+
+// Heroku PostgreSQL code
+// ######################################################################################################
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://opiryfbynhdawy:54d7d93eb3d8ca33e4365c05e38cb263c0cfaa4a6c1e9481c535e3fd8f4ec01e@ec2-54-235-146-51.compute-1.amazonaws.com:5432/d9tqp550p8taqi',
+  ssl: true,
+});
+
 client.connect();
-// console.log(client.log);
+
 let queryResults = [];
 client.query('SELECT * FROM PLAYERS;', (err, res) => {
   if (err) throw err;
@@ -31,31 +54,9 @@ client.query('SELECT * FROM PLAYERS;', (err, res) => {
   }
   // console.log(JSON.stringify(queryResults));
   client.end();
-});
-// ######################################################################################################
-
-// Heroku PostgreSQL code
-// ######################################################################################################
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL || 'postgres://opiryfbynhdawy:54d7d93eb3d8ca33e4365c05e38cb263c0cfaa4a6c1e9481c535e3fd8f4ec01e@ec2-54-235-146-51.compute-1.amazonaws.com:5432/d9tqp550p8taqi',
-//   ssl: true,
-// });
-
-// client.connect();
 
 // let qh = new QueryHandler(client);
 // qh.getAndParsePlayerStats();
-
-// Select query to get the data and transform it so we can send it to the frontend
-// client.query('SELECT * FROM PLAYERS;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
 
 
 // ######################################################################################################
